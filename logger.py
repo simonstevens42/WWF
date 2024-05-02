167870cb25b3174148f47ab44bf2ecee8d576d14
Simon Stevens <mail.simon.stevens@proton.me>
2024-05-02 09:20:30 +0200

    log basic config
    basic config aus der Methode genommen das, diese nur einmal initialisiert wird
    
    + readme hinzugefügt

--- logger.py
+++ logger.py
@@ -2,11 +2,12 @@ import logging
 from datetime import datetime
 import inspect
 
+logging.basicConfig(filename='logfile.log', encoding='utf-8', level=logging.INFO)
+
 
 def log_msg(msg, level):
     caller = inspect.stack()[1].filename
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='logfile.log', encoding='utf-8', level=logging.INFO)
     msg_strg = F"{datetime.now()} - {caller} - {msg}"
     if "INFO" in level:
         logger.info(msg_strg)
